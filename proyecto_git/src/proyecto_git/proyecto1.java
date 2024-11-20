d8e87c66b7a8b64462b5e068d3124acb148fd61e
Anabel Bueno Arrabal <abuearr2811@g.educaand.es>
2024-11-20 09:45:36 +0100

    Proyecto1 añadido

---
+++ proyecto_git/src/proyecto_git/proyecto1.java
@@ -0,0 +1,13 @@
+package proyecto_git;
+
+public class proyecto1 {
+
+	public static void main(String[] args) {
+		
+		int[] arrayNumeros = new int [20];
+		for(int i = 0; i<arrayNumeros.length; i++) {
+			int numeroRandom = (int) (Math.random()*10);
+		System.out.println(i + ":" + numeroRandom);
+		}
+		}
+}
