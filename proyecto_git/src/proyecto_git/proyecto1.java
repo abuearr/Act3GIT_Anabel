c236cc25be2bfba333d77aac00a2a15a2b42cfbc
Anabel Bueno Arrabal <abuearr2811@g.educaand.es>
2024-11-20 12:43:58 +0100

    Proyecto1 modificado en main

--- proyecto_git/src/proyecto_git/proyecto1.java
+++ proyecto_git/src/proyecto_git/proyecto1.java
@@ -4,7 +4,7 @@ public class proyecto1 {
 
 	public static void main(String[] args) {
 		
-		int[] arrayNumeros = new int [20];
+		int[] arrayNumeros = new int [30];
 		for(int i = 0; i<arrayNumeros.length; i++) {
 			int numeroRandom = (int) (Math.random()*10);
 		System.out.println(i + ":" + numeroRandom);
