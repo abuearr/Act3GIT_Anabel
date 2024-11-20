f31bb3ebc1d2c370d569424cd235c66e2f1b4784
Anabel Bueno Arrabal <abuearr2811@g.educaand.es>
2024-11-20 10:50:46 +0100

    Proyecto2 añadido

---
+++ Proyecto2_git/src/Proyecto2_git/Proyecto2.java
@@ -0,0 +1,26 @@
+package Proyecto2_git;
+
+import java.util.Scanner;
+
+public class Proyecto2 {
+	
+	public static void main(String[] args) {
+		
+		Scanner escaner = new Scanner(System.in);
+		System.out.println("Introduce el primer número: ");
+		int valor1 = Integer.parseInt(escaner.nextLine());
+		
+		System.out.println("Introduce el segundo número: ");
+		int valor2 = Integer.parseInt(escaner.nextLine());
+		
+		escaner.close(); 
+		
+		if (valor1<valor2) {
+			for (int i= valor1; i<valor2; i+=7) {  
+				System.out.println(i);
+			}
+		}
+	}
+}
+
+
