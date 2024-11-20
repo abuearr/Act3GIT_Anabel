20dd5a353178c2e8a96ea0d606df074700816dd6
Anabel Bueno Arrabal <abuearr2811@g.educaand.es>
2024-11-20 12:00:26 +0100

    Modifico Proyecto2

--- Proyecto2_git/src/Proyecto2_git/Proyecto2.java
+++ Proyecto2_git/src/Proyecto2_git/Proyecto2.java
@@ -13,12 +13,18 @@ public class Proyecto2 {
 		System.out.println("Introduce el segundo número: ");
 		int valor2 = Integer.parseInt(escaner.nextLine());
 		
+		System.out.println("Introduce el tercer número: ");
+		int valor3 = Integer.parseInt(escaner.nextLine());
+		
 		escaner.close(); 
 		
 		if (valor1<valor2) {
 			for (int i= valor1; i<valor2; i+=7) {  
 				System.out.println(i);
 			}
+		if ((valor3>valor1) && (valor3<valor2)){
+			System.out.println(valor3);
+		}
 		}
 	}
 }
